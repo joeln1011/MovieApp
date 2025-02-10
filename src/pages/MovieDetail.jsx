5fa111b1bfce45ed49ffa36e0dda2b6b33d6f613
joeln1011 <joelnt1011@gmail.com>
2025-02-10 09:05:19 -0500

    init

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,95 @@
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CircularProgressBar from "../components/CircularProgressBar";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { groupBy } from "lodash";
+
+const MovieDetail = () => {
+  const { id } = useParams();
+  const [movieInfo, setMovieInfo] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?append_to_response=release_dates,credits`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhM2M1NjkzNWY3NWRkMGI1MjQ0MTgxYTFmNzRlZWRlYiIsIm5iZiI6MTczODg4NDUzMi4zNDUsInN1YiI6IjY3YTU0NWI0ZGYzMzZmMzhhYjg1ZDY1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iX_x6Rl2bS_91sRHl_iPYmGrW9If48SSB495H0zAYrI",
+        },
+      },
+    ).then(async (res) => {
+      const data = await res.json();
+      console.log({ data });
+      setMovieInfo(data);
+    });
+  }, [id]);
+
+  const certification = (
+    (movieInfo.release_dates?.results || []).find(
+      (result) => result.iso_3166_1 === "US",
+    )?.release_dates || []
+  ).find((releaseDate) => releaseDate.certification)?.certification;
+
+  const crews = (movieInfo.credits?.crew || [])
+    .filter((crew) => ["Director", "Screenplay", "Writer"].includes(crew.job))
+    .map((crew) => ({ id: crew.id, job: crew.job, name: crew.name }));
+
+  const groupedCrews = groupBy(crews, "job");
+  console.log({ crews, groupedCrews });
+  return (
+    <div className="relative overflow-hidden text-white">
+      <img
+        className="absolute inset-0 brightness-[.2]"
+        src={`https://image.tmdb.org/t/p/original${movieInfo.backdrop_path}`}
+      />
+      <div className="relative mx-auto flex max-w-screen-xl gap-6 px-6 py-10 lg:gap-8">
+        <div className="flex-1">
+          <img
+            src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movieInfo.poster_path}`}
+          />
+        </div>
+        <div className="flex-[2] text-[1.2vw]">
+          <p className="mb-2 text-[2vw] font-bold">{movieInfo.title}</p>
+          <div className="flex items-center gap-4">
+            <span className="border border-gray-400 p-1 text-gray-400">
+              {certification}
+            </span>
+            <p>{movieInfo.release_date}</p>
+            <p>
+              {(movieInfo.genres || []).map((genre) => genre.name).join(", ")}
+            </p>
+          </div>
+          <div className="mt-4 flex items-center gap-4">
+            <div className="flex items-center gap-2">
+              <CircularProgressBar
+                percent={Math.round(movieInfo.vote_average * 10)}
+                size={3.5}
+                strokeWidth={0.3}
+              />
+              Rating
+            </div>
+            <button>
+              <FontAwesomeIcon icon={faPlay} className="mr-1" />
+              Trailer
+            </button>
+          </div>
+          <div className="mt-4">
+            <p className="mb-2 text-[1.3vw] font-bold">Overview</p>
+            <p>{movieInfo.overview}</p>
+          </div>
+          <div className="mt-4 grid grid-cols-2 gap-2">
+            {Object.keys(groupedCrews).map((job) => (
+              <div key={job}>
+                <p className="font-bold">{job}</p>
+                <p>{groupedCrews[job].map((crew) => crew.name).join(", ")}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MovieDetail;
